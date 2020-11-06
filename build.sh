a6029edaaf3637bbbd38f306a5d4f8b29dac93de
chenk <chenk@apical.com.cn>
2020-11-06 06:28:09 +0000

    修改 ffvmos 目录名为 libffvm, 修改相关配置文件

--- build.sh
+++ build.sh
@@ -4,18 +4,18 @@ set -e
 
 case "$1" in
 "")
-    make -C $PWD/ffvmos
+    make -C $PWD/libffvm
     make -C $PWD/2048
     make -C $PWD/snack
     ;;
 clean)
-    make -C $PWD/ffvmos clean
-    make -C $PWD/2048   clean
-    make -C $PWD/snack  clean
+    make -C $PWD/libffvm clean
+    make -C $PWD/2048    clean
+    make -C $PWD/snack   clean
     ;;
 distclean)
-    make -C $PWD/ffvmos clean
-    make -C $PWD/2048   clean
-    make -C $PWD/snack  clean
+    make -C $PWD/libffvm clean
+    make -C $PWD/2048    clean
+    make -C $PWD/snack   clean
     ;;
 esac
