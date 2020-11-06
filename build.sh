da7567ae6a55e302d04633e2958b344a87ee3fb0
chenk <chenk@apical.com.cn>
2020-11-06 07:29:28 +0000

    增加 bricks 俄罗斯方块小游戏

--- build.sh
+++ build.sh
@@ -7,15 +7,18 @@ case "$1" in
     make -C $PWD/libffvm
     make -C $PWD/2048
     make -C $PWD/snack
+    make -C $PWD/bricks
     ;;
 clean)
     make -C $PWD/libffvm clean
     make -C $PWD/2048    clean
     make -C $PWD/snack   clean
+    make -C $PWD/bricks  clean
     ;;
 distclean)
     make -C $PWD/libffvm clean
     make -C $PWD/2048    clean
     make -C $PWD/snack   clean
+    make -C $PWD/bricks  clean
     ;;
 esac
