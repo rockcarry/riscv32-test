33c7d05ea53793bb7ca0bd88944c0738fb28113d
chenk <chenk@apical.com.cn>
2021-12-04 10:28:47 +0800

    update snake.

--- snake/snake.c
+++ snake/snake.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include "libffvm.h"
 
-static void snack_feed(char *board, int bw, int bh, int ssize) {
+static void snake_feed(char *board, int bw, int bh, int ssize) {
     int rf = rand() % ((bw - 2) * (bh - 2) - ssize), i;
     for (i=0; i<bw*bh; i++) {
         if (board[i] == ' ' && !rf--) {
@@ -12,18 +12,18 @@ static void snack_feed(char *board, int bw, int bh, int ssize) {
     }
 }
 
-static void snack_init(char *board, int bw, int bh, int *snack, int *shead, int *stail, int *ssize) {
+static void snake_init(char *board, int bw, int bh, int *snake, int *shead, int *stail, int *ssize) {
     int i; memset(board, ' ', bw * bh);
     for (i=0; i<bw-2; i++) board[i+1] = board[bw*bh - i - 2] = '-';
     for (i=0; i<bh-2; i++) board[bw+i*bw] = board[bw-1+bw+i*bw] = '|';
     board[0] = board[bw-1] = board[bw*(bh-1)] = board[bw*bh-1] = '+';
     board[bw/2 + bh/2*bw] = '@';
     *ssize = *shead = *stail = 1;
-    snack[1] = bw * bh / 2;
-    snack_feed(board, bw, bh, *ssize);
+    snake[1] = bw * bh / 2;
+    snake_feed(board, bw, bh, *ssize);
 }
 
-static void snack_draw(char *board, int bw, int bh) {
+static void snake_draw(char *board, int bw, int bh) {
     int i, j;
     gotoxy(0, 0);
     printf("i-UP, k-DOWN, j-LEFT, l-RIGHT, q-EXIT\n\n");
@@ -33,9 +33,9 @@ static void snack_draw(char *board, int bw, int bh) {
     }
 }
 
-static int snack_move(char *board, int bw, int bh, int *snack, int *shead, int *stail, int *ssize, int d) {
-    int hx = snack[*shead] % bw, tx = snack[*stail] % bw, food;
-    int hy = snack[*shead] / bw, ty = snack[*stail] / bw;
+static int snake_move(char *board, int bw, int bh, int *snake, int *shead, int *stail, int *ssize, int d) {
+    int hx = snake[*shead] % bw, tx = snake[*stail] % bw, food;
+    int hy = snake[*shead] / bw, ty = snake[*stail] / bw;
 
     switch (d) {
     case 'J': case 'j': hx--; break; // left
@@ -50,31 +50,31 @@ static int snack_move(char *board, int bw, int bh, int *snack, int *shead, int *
     }
 
     food = (board[hx + hy * bw] == 'f');
-    board[snack[*shead]] = '#';
+    board[snake[*shead]] = '#';
     (*shead)++; *shead  %= bw * bh;
-    snack[*shead] = hy * bw + hx;
-    board[snack[*shead]] = '@';
+    snake[*shead] = hy * bw + hx;
+    board[snake[*shead]] = '@';
     if (food) {
         if (++(*ssize) == (bw - 2) * (bh - 2)) return 'W'; // 'W' means win
-        snack_feed(board, bw, bh, *ssize);
+        snake_feed(board, bw, bh, *ssize);
         return 'E'; // means snake eat
     } else {
-        if (hx != tx || hy != ty) board[snack[*stail]] = ' ';
+        if (hx != tx || hy != ty) board[snake[*stail]] = ' ';
         (*stail)++; *stail %= bw * bh;
         return 'M'; // means snake move
     }
 }
 
 int main(void) {
-    int  width = 17, height = 11, snack[width * height], shead = 0, stail = 0, ssize = 0, op = 0, reinit = 1, ret;
+    int  width = 17, height = 11, snake[width * height], shead = 0, stail = 0, ssize = 0, op = 0, reinit = 1, ret;
     char board[width * height];
     do {
         if (reinit) {
-            snack_init(board, width, height, snack, &shead, &stail, &ssize);
+            snake_init(board, width, height, snake, &shead, &stail, &ssize);
             reinit = 0; op = 0;
         }
-        ret = snack_move(board, width, height, snack, &shead, &stail, &ssize, op);
-        snack_draw(board, width, height);
+        ret = snake_move(board, width, height, snake, &shead, &stail, &ssize, op);
+        snake_draw(board, width, height);
         switch (ret) {
         case 'D': reinit = 1; printf("\ngame over !\n\npress any key to try agin ...\n"); break;
         case 'W': reinit = 1; printf("\nyou win !\n\nYou are a clever and great snake !\n\npress any key to try agin ...\n"); break;
