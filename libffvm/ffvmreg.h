a4459d9071f9d5b23403122bb2b5f4c2325579b6
chenk <chenk@apical.com.cn>
2024-02-10 19:23:37 +0800

    修改 ffvmreg 寄存器定义

--- libffvm/ffvmreg.h
+++ libffvm/ffvmreg.h
@@ -1,13 +1,13 @@
 #ifndef __FFVMREG_H__
 #define __FFVMREG_H__
 
-#define REG_FFVM_STDIO  ((volatile uint32_t*)0xF0000000)
-#define REG_FFVM_STDERR ((volatile uint32_t*)0xF0000004)
-#define REG_FFVM_GETCH  ((volatile uint32_t*)0xF0000008)
-#define REG_FFVM_KBHIT  ((volatile uint32_t*)0xF000000C)
+#define REG_FFVM_STDIO  ((volatile uint32_t*)0xFF000000)
+#define REG_FFVM_STDERR ((volatile uint32_t*)0xFF000004)
+#define REG_FFVM_GETCH  ((volatile uint32_t*)0xFF000008)
+#define REG_FFVM_KBHIT  ((volatile uint32_t*)0xFF00000C)
 
-#define REG_FFVM_MSLEEP ((volatile uint32_t*)0xF0000100)
-#define REG_FFVM_CLRSCR ((volatile uint32_t*)0xF0000104)
-#define REG_FFVM_GOTOXY ((volatile uint32_t*)0xF0000108)
+#define REG_FFVM_MSLEEP ((volatile uint32_t*)0xFF000100)
+#define REG_FFVM_CLRSCR ((volatile uint32_t*)0xFF000104)
+#define REG_FFVM_GOTOXY ((volatile uint32_t*)0xFF000108)
 
 #endif
