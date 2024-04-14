0ce510289b18fc0d72cb5b2040e4f27f9de951ca
chenk <chenk@apical.com.cn>
2024-04-14 21:16:42 +0800

    修改 time.c 增加 time 和 localtime 函数的测试代码

--- time/time.c
+++ time/time.c
@@ -1,12 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 #include "ffvmreg.h"
 #include "libffvm.h"
 
 int main(void)
 {
     while (1) {
-        printf("tick: %lu, time: %lu\n", *REG_FFVM_TICKTIME, *REG_FFVM_REALTIME);
+        time_t     tt = time(NULL);
+        struct tm *tm = localtime(&tt);
+        printf("tick: %lu, time: %lu, %04d-%02d-%02d %02d-%02d-%02d\n", *REG_FFVM_TICKTIME, *REG_FFVM_REALTIME,
+            tm->tm_year + 1900, tm->tm_mon + 1, tm->tm_mday, tm->tm_hour, tm->tm_min, tm->tm_sec);
         mdelay(1000);
     }
     return 0;
